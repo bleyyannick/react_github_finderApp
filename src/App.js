9d66f37d2679a7bc292b55e0ec1d50a48cc55b95
Yannick Bley <bleyyannickpro@gmail.com>
2021-12-30 10:12:45 +0100

    home page layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import {BrowserRouter as Router, Route } from 'react-router-dom'; 
 
-
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer'; 
 
 function App() {
   return (
-    <div className="App">
-         <button class="btn btn-primary">daisyUI Button</button>
-    </div>
+    <Router>
+         <div className="flex flex-col justify-between h-screen">
+              <Navbar />
+              <main className="container mx-auto px-3 pb-12">
+                Contenu
+              </main>
+              <Footer />
+         </div>
+    </Router>
   );
 }
 
