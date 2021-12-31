d11554bdbd5c05a8f987f043f38ba89a14bcf10d
Yannick Bley <bleyyannickpro@gmail.com>
2021-12-31 09:50:53 +0100

    display user items profile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import {BrowserRouter as Router, Route } from 'react-router-dom'; 
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'; 
 
 import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer'; 
+import Footer from './components/layout/Footer';
+import NotFound from './pages/NotFound'; 
+import About from './pages/About'; 
+import Home from './pages/Home'; 
 
 function App() {
   return (
@@ -9,7 +12,14 @@ function App() {
          <div className="flex flex-col justify-between h-screen">
               <Navbar />
               <main className="container mx-auto px-3 pb-12">
-                Contenu
+                <Routes>
+                  <Route path="/" element={<Home />}/>
+                  <Route path="/about" element={<About />}/>
+                  <Route path="/notfound" element={<NotFound />}/>
+                  <Route path="/*" element={<NotFound />}/>
+
+                 
+                </Routes>
               </main>
               <Footer />
          </div>
---
+++ src/pages/About.jsx
@@ -0,0 +1,23 @@
+
+
+const About = () => {
+   return (
+    <>
+    <h1 className='text-6xl mb-4'>Github Finder</h1>
+        <p className='mb-4 text-2xl font-light'>
+         Ceci est petite application React qui permet de retouver les profils Github. 
+        </p>
+        <p className='text-lg text-gray-400'>
+        Version <span className='text-white'>1.0.0</span>
+        </p>
+        <p className='text-lg text-gray-400'>
+        Le style de cette application est à mettre au crédit de  
+        <a className='text-white' href='https://twitter.com/hassibmoddasser'>
+            Hassib Moddasser
+        </a>
+        </p>
+  </>
+   )
+}
+
+export default About; 
